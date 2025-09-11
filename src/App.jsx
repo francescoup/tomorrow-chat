82e4a6b280db0bd2049b5928c010079436409955
Cristian <ccristian02@gmail.com>
2025-09-11 19:18:08 +0200

    Feat: icon button
    creato e testato un iconwrapper con aggiunte icone per testare

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import IconWrapper from "./components/IconWrapper";
+import { icons } from './data/icons';
+
+const iconsMap = icons.reduce((acc, iconObj) => {
+  acc[iconObj.label] = iconObj;
+  return acc;
+}, {});
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="flex items-center justify-center min-h-screen w-screen bg-gray-100">
-        <p className="text-4xl text-blue-800">Tomorrow dev chat</p>
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen bg-gray-100">
+      <div className="mb-6">
+        <p className="flex items-center gap-2 text-4xl text-blue-800 mb-2">
+          Tomorrow dev chat
+          <IconWrapper icon={iconsMap.bot.icon} onClick={iconsMap.bot.onClick} />
+        </p>
       </div>
-    </>
+      <div className="text-center">
+        <p className="text-4xl text-blue-800 mb-2">
+          Building projects
+          <IconWrapper icon={iconsMap.rocket.icon} onClick={iconsMap.rocket.onClick} />
+        </p>
+      </div>
+    </div>
   );
 }
 
