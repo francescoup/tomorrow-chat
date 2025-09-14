7aff68d666d44590aa915295e4c8529bb504d8c2
MicOrl <139447703+micheleorl@users.noreply.github.com>
2025-09-14 20:13:37 +0200

    Creato componente TextInput riutilizzabile per campo input chat che accetta value, onChange e placeholder come da props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import TextInput from "./Components/TextInput";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const [message, setMessage] = useState(""); // ! TEXT INPUT ! Necessario, altrimenti il componente non viene inizializzato e viene restituito l'errore "ReferenceError".
+
+  // Piccola simulazione "visiva" dell'input test
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen w-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center min-h-screen w-screen bg-gray-100 p-4 space-y-4">
         <p className="text-4xl text-blue-800">Tomorrow dev chat</p>
+
+        <div className="w-full max-w-md">
+          <TextInput
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Scrivi un messaggio..."
+          />
+        </div>
       </div>
     </>
   );
