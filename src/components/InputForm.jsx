9e3f267be91c4715031c8965b1886db02875fb3f
francesco mangione <f.mangione@live.com>
2025-09-19 12:52:30 +0200

    feat:add dark mode function

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -4,7 +4,7 @@ import { TbSend2 } from "react-icons/tb";
 
 export default function InputForm({ input, setInput, onSend }) {
   return (
-    <div className="p-4 rounded-xl shadow bg-white m-4 border-1 border-gray-200">
+    <div className="p-4 rounded-xl shadow bg-white dark:bg-[#18192D] m-4 border-1 border-gray-200 dark:border-gray-700 dark:text-white">
       <div className="flex items-center">
         <TextInput
           value={input}
