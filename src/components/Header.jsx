63674175c052fcd151cda6317877e816beaa24f8
Cristian <ccristian02@gmail.com>
2025-09-19 17:19:37 +0200

    chore: header dark mode
    Aggiunta la dark mode e cambiato leggermente l'hover dell'iconwrapper, rifiniture comunque da decidere

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import IconWrapper from "./IconWrapper";
 import { FaSun, FaMoon } from "react-icons/fa";
 
-export default function Header({ light, dark }) {
-  // Per gestire il cambio tema, per ora sono placeholder
-  const handleLight = () => alert("Light mode!");
-  const handleDark = () => alert("Dark mode!");
+export default function Header() {
+  const [dark, setDark] = useState(false);
 
   return (
-    <header className="w-full h-[88px] px-6 flex items-center justify-between  border-b border-gray-200 bg-gray-100">
-      <h1 className="text-xl font-bold text-gray-800">Tomorrow dev chat</h1>
-      <div className="flex gap-3 h-[40px] items-center">
-        <IconWrapper icon={<FaSun />} onClick={light} aria-label="Light mode" />
-        <IconWrapper icon={<FaMoon />} onClick={dark} aria-label="Dark mode" />
+    <header className={`w-full h-[88px] px-6 flex items-center justify-between border-b ${
+        dark
+          ? "bg-[#20233C] border-gray-700 text-gray-100"
+          : "bg-white border-gray-200 text-gray-800"
+      }`}
+    >
+      <h1 className="text-xl font-bold select-none">Tomorrow dev chat</h1>
+      <div className="flex items-center gap-2 h-[40px] ">
+        <IconWrapper
+          icon={dark ? <FaSun className="text-gray-200" /> : <FaMoon className="text-gray-800" />}
+          onClick={() => setDark((d) => !d)}
+          // la proprietà dark:hover non è funzionante 
+          className="bg-transparent hover:bg-gray-400 dark:hover:bg-gray-800"
+        />
       </div>
     </header>
   );
